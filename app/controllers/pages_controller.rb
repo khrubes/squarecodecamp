7cb9165952cd5f06fd2762bf28f5592fff45c0c1
Hashma <shahi22h@mtholyoke.edu>
2016-01-08 11:15:42 -0800

    Rachel: fixed controller for resources page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,37 +6,21 @@ class PagesController < ApplicationController
   end
 
   def resources 
-  	@resource = Resource.new
-  	@resources = Resource.all
-
   end
 
   def new 
-  	@resource= Resource.new
-  	respond_with @resource
-
   end
 
   def create 
-  	@resource = Resource.create(res_params)
-  	respond_with @resources
-  	
   end
 
   def edit
-  	respond_with @resource
-
   end
 
   def update
-  	@resource.update(res_params)
-  	respond_with @resource
   end
 
   def destroy
-  	@resource.destroy
-  	respond_with @resource
-
   end
 
   def res_params
